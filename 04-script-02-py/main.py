35bdf64e6e35598f6d1780d9a64855b9888951ec
Alexander Selyukov <asexsela@gmail.com>
2022-05-24 02:06:27 +0300

    fix

--- 04-script-02-py/main.py
+++ 04-script-02-py/main.py
@@ -21,34 +21,34 @@ import json
 #         print(os.path.abspath(prepare_result))
 
 
-# print("Укажите абсолютный путь к репозиторию:")
-# path = input('> ')
+print("Укажите абсолютный путь к репозиторию:")
+path = input('> ')
 
 
-# os.chdir(path)
-# bash_command = ["git status"]
-# result_os = os.popen(' && '.join(bash_command)).read()
+os.chdir(path)
+bash_command = ["git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
 
-# for result in result_os.split('\n'):
-#     if result.find('modified') != -1:
-#         prepare_result = result.replace('\tmodified:   ', '')
-#         print('\n'+path+prepare_result)
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '/')
+        print('\n'+path+prepare_result)
 
 
-conf_file="./04-script-02-py/config.json"
+# conf_file="./04-script-02-py/config.json"
 
-with open(conf_file) as json_data_file:
-    conf = json.load(json_data_file)
+# with open(conf_file) as json_data_file:
+#     conf = json.load(json_data_file)
 
-for host, ip in conf.items():
-    new_ip=socket.gethostbyname(host)
+# for host, ip in conf.items():
+#     new_ip=socket.gethostbyname(host)
 
-    if (ip != new_ip):
-        print ('[ERROR] {} IP mismatch: {} {}'.format(host,ip,new_ip))
-        conf[host]=new_ip
+#     if (ip != new_ip):
+#         print ('[ERROR] {} IP mismatch: {} {}'.format(host,ip,new_ip))
+#         conf[host]=new_ip
 
-for host, ip in conf.items():
-    print('{} - {}'.format(host,ip))
+# for host, ip in conf.items():
+#     print('{} - {}'.format(host,ip))
 
-with open(conf_file, "w") as json_data_file:
-    json.dump(conf, json_data_file, indent=2)
+# with open(conf_file, "w") as json_data_file:
+#     json.dump(conf, json_data_file, indent=2)
