c8a75a951b52f344b8f79b2526354d93fd2d9829
Alexander Selyukov <asexsela@gmail.com>
2022-07-18 23:20:37 +0300

    add 05-virt-05-docker-swarm

---
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gm0jl0etfpqkvfboib"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g62c4ktk6acafamug7"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd83i289pb7qhj4ti37u"
+}
